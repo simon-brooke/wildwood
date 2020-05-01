d539caf25ab64bc2ba709ccd492e3f7e0b7e166e
Simon Brooke <simon@journeyman.cc>
2020-05-01 10:38:15 +0100

    Added cloverage output to archive

--- project.clj
+++ project.clj
@@ -1,19 +1,21 @@
 (defproject wildwood "0.1.0-SNAPSHOT"
-  :description "A general inference library using a game theoretic inference mechanism."
-  :url "http://example.com/FIXME"
-  :license {:name "GNU General Public License,version 2.0 or (at your option) any later version"
-            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [com.taoensso/timbre "4.10.0"]
-                 [com.novemberain/monger "3.1.0"]
-                 [prismatic/schema "1.1.12"]]
+  :cloverage {:output "docs/cloverage"}
   :codox {:metadata {:doc "**TODO**: write docs"
                      :doc/format :markdown}
           :output-path "docs/codox"
           :source-uri "https://github.com/simon-brooke/the-great-game/blob/master/{filepath}#L{line}"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.novemberain/monger "3.1.0"]   ;; not yet used
+                 [prismatic/schema "1.1.12"]]       ;; not yet used
+  :description "A general inference library using a game theoretic inference mechanism."
+  :license {:name "GNU General Public License,version 2.0 or (at your option) any later version"
+            :url "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html"}
   :plugins [[lein-cloverage "1.1.1"]
             [lein-codox "0.10.7"]
             [lein-cucumber "1.0.2"]
             [lein-gorilla "0.4.0"]]
-  :repl-options {:init-ns wildwood.core})
+  :repl-options {:init-ns wildwood.core}
+  :url "https://simon-brooke.github.io/wildwood/"
+  )
